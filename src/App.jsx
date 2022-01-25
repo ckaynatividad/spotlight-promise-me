86f97f7e38ba2a4bbb49d60878720d750e1b2b87
ckaynatividad <natividad.ckay@gmail.com>
2022-01-25 11:52:30 -0800

    four apis done fetch all working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { randomTen, randomTen2 } from "./services/promise-me";
+import { useEffect } from "react";
+import { fetchArt, fetchArt2, fetchEmoji, fetchEmoji2, fetchGhibli, fetchGhibli2, fetchNeko, fetchNeko2, randomTen, randomTen2 } from "./services/promise-me";
 
 export default function App() {
-  
-  console.log(randomTen())
-  console.log(randomTen2())
+  useEffect(() => {
+   randomTen2();
+   randomTen2()
+
+   fetchNeko()
+   fetchNeko2()
+
+   fetchEmoji()
+   fetchEmoji2()
+
+   fetchArt()
+   fetchArt2()
+  });
+
   return <h1>Hello World</h1>
 
-  ;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -8,26 +8,59 @@ import { data } from "autoprefixer"
 export async function randomTen() {
     const resp = await fetch('https://animechan.vercel.app/api/quotes')
     const data = await resp.json()
-    return data
+    return console.log(data)
 }
 
 export function randomTen2() {
     const resp = fetch('https://animechan.vercel.app/api/quotes')
     .then(resp => resp.json())
+    .then(quotes => console.log(quotes))
+    return resp
+}
+/**
+ * TODO:
+ */
+export async function fetchNeko() {
+    const resp = await fetch('https://nekos.best/api/v1/nekos')
+    const data = await resp.json()
+    return console.log(data);
+}
+export function fetchNeko2() {
+    const resp = fetch('https://nekos.best/api/v1/nekos')
+    .then(resp => resp.json())
+    .then(nekos => console.log(nekos))
+    return resp;
+}
+
+
+/**
+ * TODO:
+ */
+export async function fetchEmoji() {
+    const resp = await fetch('https://emojihub.herokuapp.com/api/all')
+    const data = await resp.json()
+    return console.log(data)
+}
+export function fetchEmoji2() {
+    const resp = fetch('https://emojihub.herokuapp.com/api/all')
+    .then(resp => resp.json())
+    .then(films => console.log(films))
     return resp;
 }
 /**
  * TODO:
  */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
+export async function fetchArt() {
+    const resp = await fetch('https://emojihub.herokuapp.com/api/all')
+    const data = await resp.json()
+    return console.log(data)
+}
+export function fetchArt2() {
+    const resp = fetch('https://emojihub.herokuapp.com/api/all')
+    .then(resp => resp.json())
+    .then(art => console.log(art))
+    return resp
+}
 /**
  * TODO:
  */
